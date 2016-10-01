dfce509966c4fe6c9ff77cfb15df0c5fea4db0f4
Florent <florent.jacquemard@inria.fr>
2016-10-01 12:30:14 +0200

    first commit

--- qparse/main.cpp
+++ qparse/main.cpp
@@ -2,6 +2,8 @@
 //  main.cpp
 //  qparse
 //
+// https://github.com/florento/q-parse
+//
 //  Created by Florent Jacquemard on 01/10/16.
 //  Copyright © 2016 Florent Jacquemard. All rights reserved.
 //
@@ -13,3 +15,4 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     return 0;
 }
+
