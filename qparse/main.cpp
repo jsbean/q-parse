b354d6c9d377ace143814452ee58bd3fa7c09cb6
Florent <florent.jacquemard@inria.fr>
2016-10-31 11:54:20 +0100

    clock for benchmarking

--- qparse/main.cpp
+++ qparse/main.cpp
@@ -8,11 +8,8 @@
 //  Copyright © 2016 Florent Jacquemard. All rights reserved.
 //
 
-
-
-
-
 #include <stdio.h>
+#include <time.h>
 #include <iostream>
 #include <assert.h>
 #include <string>
@@ -22,8 +19,15 @@
 #include "Kbest.hpp"
 
 
+double duration(clock_t start)
+{
+    return ((double)(clock() - start)/CLOCKS_PER_SEC * 1000);
+}
+
 int main(int argc, const char * argv[])
 {
+    clock_t time_start;
+    clock_t time_end;
     
 // test WTA file IO
     assert (argc >= 2);
@@ -33,11 +37,18 @@ int main(int argc, const char * argv[])
 //    ta->print();  // cout << ta;
 
     cout << "\n==== Clean schema:\n";
+    time_start = clock();
     ta->clean();
+    //time_end = clock();
+    cout << "time to clean WTA : ";
+    cout << duration(time_start) << "ms \n";
     ta->print();  // cout << ta;
 
+    time_start = clock();
     unsigned int res = ta->resolution();
-    std::cout << "\n Resolution = " << res << '\n';
+    cout << "time to compute resolution : ";
+    cout << duration(time_start) << "ms \n";
+    std::cout << "\nresolution = " << res << '\n';
 
 // test Combo construction
     if (argc == 2) return 0;
@@ -46,20 +57,27 @@ int main(int argc, const char * argv[])
     std::cout << " segment size " << seg.size() << '\n';
 
     std::cout << "\n==== COnstruction COmbo\n";
+    time_start = clock();
     ComboWTA* combo = new ComboWTA(seg, *ta, 0);
+    cout << "time to compute ComboWTA : ";
+    cout << duration(time_start) << "ms \n";
 
     delete ta;
 
-    cout << "\n Combo:\n";
+    cout << "\nCombo:\n";
     combo->print();
     
     cout << "\n==== Clean Combo:\n";
+    time_start = clock();
     combo->clean();
+    cout << "time to clean ComboWTA : ";
+    cout << duration(time_start) << "ms \n";
     combo->print();  // cout << ta;
 
     
 // test k-best
-    cout << "\n==== 1-best\n";
+    cout << "\n==== 1-best (" << combo->initials.size() << " initials)\n";
+    time_start = clock();
     Ktable<WeightMin> kt = Ktable<WeightMin>(combo);
     for (set<State>::iterator i = combo->initials.begin();
          i != combo->initials.end(); ++i)
@@ -68,6 +86,8 @@ int main(int argc, const char * argv[])
         Run r = kt.best(s, 1);
         cout << "weight 1-best[" << s << "] = " << r.weight << "\n";
     }
+    cout << "time to compute best for initial states ComboWTA : ";
+    cout << duration(time_start) << "ms \n";
     
 //    Run r = kt.best(1);
 //    cout << "weight 1-best = " << r.weight << "\n";
