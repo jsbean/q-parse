7ab321eacacafb76844c2ae1837aebbfe6af4543
Florent <florent.jacquemard@inria.fr>
2016-11-18 11:32:17 +0100

    target schemas

--- schemas/schemas.cpp
+++ schemas/schemas.cpp
@@ -1,11 +1,15 @@
 //
-//  schemas.cpp
+//  quant.cpp
 //  qparse
 //
-//  Created by Florent Jacquemard on 17/11/16.
+// https://github.com/florento/q-parse
+//
+//  Created by Florent Jacquemard on 01/10/16.
 //  Copyright © 2016 Florent Jacquemard. All rights reserved.
 //
-// management of schema files
+// terminal command line for
+// rhythm quantization with qparse library
+
 
 #include <stdio.h>
 #include <time.h>
@@ -14,6 +18,7 @@
 #include <string>
 #include <vector>
 
+#include "ComboWTA.hpp"
 #include "Kbest.hpp"
 #include "SubdivisionSchema.hpp"
 
@@ -23,16 +28,13 @@ double duration(clock_t start)
     return ((double)(clock() - start)/CLOCKS_PER_SEC * 1000);
 }
 
-
-
-
 int main(int argc, const char * argv[])
 {
     
     clock_t time_start;
     //clock_t time_end;
     
-    // test WTA file input
+    // test WTA file IO
     assert (argc >= 2);
     std::cout << "\n==== Read schema WTA from " << argv[1] << '\n';
     
@@ -68,19 +70,61 @@ int main(int argc, const char * argv[])
     cout << "time to 1-best (max) for initial state of schema : ";
     cout << duration(time_start) << "ms \n";
     
-    // test normalization schema
-
-    
-    // test WTA file output
+    // test Combo construction
     if (argc == 2) return 0;
-    std::cout << "\n==== Write normalized WTA to " << argv[2] << '\n';
-    schema->save(argv[2]);
+    std::cout << "\n==== Read input segment from " << argv[2] << '\n';
+    Segment seg = Segment(argv[2], res);
+    std::cout << "segment size " << seg.size() << '\n';
+    
+    std::cout << "\n==== COnstruction COmbo\n";
+    time_start = clock();
+    ComboWTA* combo = new ComboWTA(seg, *schema, 0);
+    cout << "time to compute ComboWTA : ";
+    cout << duration(time_start) << "ms \n";
+    
+    delete schema;
+    
+    cout << "\nCombo:\n";
+    combo->print();
+    cout << *combo;
+    
+    //    cout << "\n==== Clean Combo:\n";
+    //    time_start = clock();
+    //    combo->clean();
+    //    cout << "time to clean ComboWTA : ";
+    //    cout << duration(time_start) << "ms \n";
+    //    combo->print();
+    //    cout << *combo;
+    
+    
+    
+    // test k-best for initial states of Combo
+    int k = 30;
+    time_start = clock();
+    Ktable<WeightMin> kkt = Ktable<WeightMin>(combo);
+    
+    for (size_t pre = 0; pre <= combo->max_pre(); pre++)
+    {
+        for (size_t post = 0; post <= combo->max_post(); post++)
+        {
+            cout << "\n==== " << k << "-best for ";
+            cout << "pre=" << pre << " post=" << post << "\n";
+            for (int i = k; i > 0; i--)
+            {
+                Run r = kkt.best(pre, post, i);
+                if (! r.unknown())
+                {
+                    cout << "weight " << i << "-best = " << r.weight;
+                    cout << " " << r.duration << "\n";
+                }
+            }
+        }
+    }
+    
+    cout << "\ntime for " << k << "-best for all initial states ComboWTA : ";
+    cout << duration(time_start) << "ms \n";
+    
+    delete combo;
     
     return 0;
 }
-
-
-
-
-
-
